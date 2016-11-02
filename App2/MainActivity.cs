db572b0e1bbd6326626835cc849c0f5210970526
SOFSPEEL <steve.feidelberg@gmail.com>
2016-11-02 11:39:24 -0700

    Add project files.

---
+++ App2/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Acr.UserDialogs;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Portable;
+
+namespace App2
+{
+    [Activity(Label = "App2", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        private readonly Class1 _class1 = new Class1();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            UserDialogs.Init(this);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            var button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate
+            {
+                button.Text = $"{count++} clicks!";
+                _class1.EditMediaCommand.Execute();
+
+            };
+        }
+    }
+}
+
---
+++ Portable/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Acr.UserDialogs;
+using MvvmCross.Core.ViewModels;
+
+namespace Portable
+{
+
+
+
+    public class Class1
+    {
+        private IMvxCommand _editMediaCommand;
+
+        public IMvxCommand EditMediaCommand => _editMediaCommand ?? (_editMediaCommand = new MvxAsyncCommand(ActionAsync));
+
+
+        private static async Task ActionAsync()
+        {
+            await UserDialogs.Instance.ActionSheetAsync(null, "Cancel", null, null, "Junk1", "Junk2");
+        }
+
+    }
+}
